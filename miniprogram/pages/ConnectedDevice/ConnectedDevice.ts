43bc733e6080ca731174af9ff64a4eaf0b774136
石山岭 <shishanling.ios@qq.com>
2023-10-22 21:55:15 +0800

    页面开始搭建

---
+++ miniprogram/pages/ConnectedDevice/ConnectedDevice.ts
@@ -0,0 +1,27 @@
+// pages/ConnectedDevice/ConnectedDevice.ts
+Component({
+
+	/**
+	 * 组件的属性列表
+	 */
+	properties: {
+
+	},
+
+	/**
+	 * 组件的初始数据
+	 */
+	data: {
+
+	},
+
+	/**
+	 * 组件的方法列表
+	 */
+	methods: {
+      addDev(){
+        console.log("点击了添加设备");
+        
+      }
+	}
+})
