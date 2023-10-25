b021f141994f1194804039c95f0f99069ccb4335
石山岭 <shishanling.ios@qq.com>
2023-10-25 21:27:59 +0800

    页面搭建完毕-开始编写逻辑

--- miniprogram/pages/AddDevice/AddDevice.ts
+++ miniprogram/pages/AddDevice/AddDevice.ts
@@ -16,9 +16,9 @@ Component({
 	 */
 	data: {
     searching: false,
-    devicesList: [] as any[]
+    devicesList: [] as any[],
+    searchTime:0 
 	},
-  
 	/**
 	 * 组件的方法列表
 	 */
@@ -41,56 +41,49 @@ Component({
       //找到蓝牙设备回调
       wx.onBluetoothDeviceFound(function (devices:any) {
         //获取到设备开始获取服务ID
+        // console.log("onBluetoothDeviceFound==", devices);
+        
         //剔除重复设备，兼容不同设备API的不同返回值
         var isnotexist = true
-        if (devices.deviceId) {
-          if (devices.advertisData) {
-            devices.advertisData = app.buf2hex(devices.advertisData)
-          } else {
-            devices.advertisData = ''
-          }
-          console.log(devices)
-          for (var i = 0; i < that.data.devicesList.length; i++) {
-            if (devices.deviceId == (that.data.devicesList[i] as any).deviceId) {
-              isnotexist = false
-            }
-          }
-          if (isnotexist) {
-            //在这里获取服务uuid
-            that.data.devicesList.push(devices)
-          }
-        } else if (devices.devices) {
+       if (devices.devices.length) {
           if (devices.devices[0].advertisData) {
             devices.devices[0].advertisData = app.buf2hex(devices.devices[0].advertisData)
           } else {
             devices.devices[0].advertisData = ''
           }
-          console.log(devices.devices[0])
+          //判断列表中有没有当前设备
           for (var i = 0; i < that.data.devicesList.length; i++) {
+            console.log("(devices.devices[0].deviceId==", devices.devices[0]);
+            
             if (devices.devices[0].deviceId == that.data.devicesList[i].deviceId) {
+              console.log("1111111111111111");
               isnotexist = false
+              break;
             }
-           
-          }
-          if (isnotexist) {
-            //在这里获取服务uuid
-            that.data.devicesList.push(devices.devices[0])
-          }
-        } else if (devices[0]) {
-          if (devices[0].advertisData) {
-            devices[0].advertisData = app.buf2hex(devices[0].advertisData)
-          } else {
-            devices[0].advertisData = ''
-          }
-          console.log(devices[0])
-          for (var i = 0; i < that.data.devicesList.length; i++) {
-            if (devices[0].deviceId == that.data.devicesList[i].deviceId) {
+            if(!devices.devices[0].name?.length){
+              console.log("222222222222222");
               isnotexist = false
+              break;
+            }
+            if(!devices.devices[0].advertisServiceUUIDs?.length){
+              console.log("33333333333333333333333");
+              isnotexist = false
+              break;
             }
           }
-          if (isnotexist) {
-            //在这里获取服务uuid
-            that.data.devicesList.push(devices[0])
+          
+          console.log("isnotexist==", isnotexist);
+          //如果列表中没有 再判断 advertisServiceUUIDs 中有没有想要的设备 
+          if(devices.devices[0].advertisServiceUUIDs?.length && isnotexist){
+            let advertisServiceUUIDs = devices.devices[0].advertisServiceUUIDs
+            for (const index in advertisServiceUUIDs) {
+              const serviceUUID = advertisServiceUUIDs[index];
+              //找到 FA30就添加设备 停止循环
+              if (serviceUUID.indexOf('FA30') != -1){
+                that.data.devicesList.push(devices.devices[0])
+                break;
+              }
+            }
           }
         }
         that.setData({
@@ -130,10 +123,15 @@ Component({
         })
       }
     },
+   
     //开始搜索
     beginSearch: function () {
+
       var that = this
       if (!that.data.searching) {
+       that.data.searchTime = setTimeout(()=>{
+        that.stopSearchBLEDevices()
+        },15000)
         wx.closeBluetoothAdapter({
           complete: function (res) {
             console.log(res)
@@ -173,14 +171,8 @@ Component({
           }
         })
       } else {
-        wx.stopBluetoothDevicesDiscovery({
-          success: function (res) {
-            console.log(res)
-            that.setData({
-              searching: false
-            })
-          }
-        })
+        clearTimeout(that.data.searchTime);
+       that.stopSearchBLEDevices()
       }
     },
     onCollPencli(e:any){
@@ -214,5 +206,38 @@ Component({
         }
       })
     },
+    getDeviceServices(deviceObjc:any){
+      console.log("deviceObjc==", deviceObjc);
+      
+      let that = this;
+      wx.getBLEDeviceServices({
+        deviceId: deviceObjc.deviceId,
+        success: function (res) {
+          console.log("getBLEDeviceServices-success==",  res);
+          let tempSers = res.services
+          for (let index = 0; index < tempSers.length; index++) {
+            if (tempSers[index].uuid.indexOf('FA30') != -1){
+              that.data.devicesList.push(deviceObjc)
+              break;
+            } 
+          }
+        },
+        fail:function(error){
+          console.log('getBLEDeviceServices-error===', error);
+          
+        }
+      })
+    },
+    stopSearchBLEDevices(){
+      let that = this;
+      wx.stopBluetoothDevicesDiscovery({
+        success: function (res) {
+          console.log(res)
+          that.setData({
+            searching: false
+          })
+        }
+      })
+    }
 	}
 })
--- miniprogram/pages/ConnectedDevice/ConnectedDevice.ts
+++ miniprogram/pages/ConnectedDevice/ConnectedDevice.ts
@@ -20,11 +20,90 @@ Component({
 	 * 组件的方法列表
 	 */
 	methods: {
+    onLoad:function(){
+      this.getStorageDeviceInfo()
+    },
       addDev(){
         console.log("点击了添加设备");
         wx.navigateTo({url:'../AddDevice/AddDevice?'})
+      },
+      //event:any
+      toDeviceDetails(){
+        wx.navigateTo({url:'../DeviceDetails/DeviceDetails?'})
+      },
+      getConnectedDevices(){
+        var that = this
+        wx.getStorage({
+          key: "deviceId",
+          success(res) {
+            console.log("wx.getStorage==-success", res.data);
+            let tempList:any[] = [];
+            for (const index in res.data) {
+              const element = res.data[index];
+              console.log("element==", element);
+              if (element.length != 0){
+                tempList.push(element);
+              }
+            }
+            wx.openBluetoothAdapter({
+              success: function (res) {
+                console.log(res)
+                wx.getConnectedBluetoothDevices({
+                  services: tempList,
+                  success (res) {
+                    that.setData({
+                      devicesList:res.devices
+                    })
+                    console.log("getConnectedBluetoothDevices-success==", res)
+                  },
+                  fail(error){
+                    console.log("getConnectedBluetoothDevices-error==", error);
+                  }
+                })
+              },
+              fail: function (res) {
+                console.log('startBluetoothDevicesDiscovery error--', res)
+                wx.showModal({
+                  title: '提示',
+                  content: '请检查手机蓝牙是否打开',
+                  showCancel: false,
+                  success: function (res:any) {
+                    that.setData({
+                      searching: false
+                    })
+                  }
+                })
+              }
+            })
         
+          },
+          fail(result){
+            console.log("wx.getStorage==-fail", result);
+          }
+          
+        })
         
+      },
+      //获取本地存储的设备信息
+      getStorageDeviceInfo(){
+        var that = this
+        wx.getStorage({
+          key: "connectedDeviceList",
+          success(res) {
+            console.log("wx.getStorage==-success", res.data);
+          },
+          fail(result){
+            console.log("wx.getStorage==-fail", result);
+          }
+          
+        })
       }
 	}
 })
+
+/*
+deviceid会发生变化 不可靠 但是已经存储的设备需要使用这个ID连接
+
+那么advertisServiceUUIDs呢? 暂时定为他不会变
+
+*/
